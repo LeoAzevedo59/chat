f2c8d23c76b4c81820087097adc991dd2318428d
Léo Azevedo <74332663+LeoAzevedo59@users.noreply.github.com>
2022-05-18 23:21:28 -0300

    Update config.php

--- back-end/config.php
+++ back-end/config.php
@@ -1,5 +1,6 @@
 <?php 
-    $connection = mysqli_connect("localhost", "u953073190_leoazevedo", "", "chat");
+    $connection = mysqli_connect("localhost", "u953073190_leoazevedo", "-Leo123456", "u953073190_chat");
+    // $connection = mysqli_connect("localhost", "root", "", "chat");
     if(!$connection)
     {
         echo "Database connected" . mysqli_connect_errno();
